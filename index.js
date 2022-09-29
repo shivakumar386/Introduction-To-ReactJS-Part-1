2ea45f797b6b7ce41934daa0c6254725fa76e77c
shivakumar386 <shivakumarkantem528@gmail.com>
2022-09-29 12:05:44 +0000

    IntroductionToReactJSPart1

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="bg-container">
+    <h1 className="heading">Super Over League</h1>
+    <div className="images-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="images"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="images"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
